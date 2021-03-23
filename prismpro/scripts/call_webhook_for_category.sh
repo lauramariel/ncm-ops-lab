f228e016f56540fa4e23d55187bd5e2f261d17aa
Sarah Hernandez <sarah.hernandez@nutanix.com>
2021-03-22 18:31:21 -0700

    Updating scripts

--- prismpro/scripts/call_webhook_for_category.sh
+++ prismpro/scripts/call_webhook_for_category.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
+# Copyright (c) 2020 Nutanix Inc.  All rights reserved.
+
+# To run this script please provide the following parameters:
+# The Prism Central IP
+# The Prism Central Username
+# The Prism Central Password
+# The Webhook UUID of the Playbook you would like to trigger
+# The type of entity we are triggering the webhook for (vm, host, or cluster)
+# The category name
+# The category value
+# Example ./call_webhook_for_category.sh 1.2.3.4 admin Nutanix/4u 290d76cc-4819-4605-8013-71fd0a3307a6 vm Cat123 Test123
+
+# Note this script requires jq to be installed in your VM.
+
 PC_IP="$1"
-PC_SSH_USER="$2"
-PC_SSH_PASS="$3"
+PC_UI_USER="$2"
+PC_UI_PASS="$3"
 WEBHOOK_ID="$4"
 CATEGORY_TYPE="$5"
 CATEGORY_NAME="$6"
 CATEGORY_VALUE="$7"
 
-echo "Installing jq"
-curl -k --show-error --remote-name --location https://s3.amazonaws.com/get-ahv-images/jq-linux64.dms
-chmod u+x jq-linux64.dms
-ln -s jq-linux64.dms jq
-mv jq* ~/bin
-
 ATTRIBUTE_NAME=""
-
-ORIGINAL_CT=CATEGORY_TYPE
 
 if [ "$CATEGORY_TYPE" == "vm" ]; then
     CATEGORY_TYPE="mh_vm"
     ATTRIBUTE_NAME="vm_name"
-elif [ "$CATEGORY_TYPE" == "host" ]; then 
+elif [ "$CATEGORY_TYPE" == "host" ]; then
     CATEGORY_TYPE="host"
     ATTRIBUTE_NAME="node_name"
-elif [ "$CATEGORY_TYPE" == "cluster" ]; then 
+elif [ "$CATEGORY_TYPE" == "cluster" ]; then
     CATEGORY_TYPE="cluster"
     ATTRIBUTE_NAME="cluster_name"
 else
@@ -52,11 +58,11 @@ echo "Request Body 1 :  $REQUEST1"
 
 response=$(curl -X POST -k $GROUPS_ENDPOINT \
         --header 'Content-Type: application/json' \
-        -u $PC_SSH_USER:$PC_SSH_PASS \
-        --data-raw "$REQUEST1"      
-        )    
+        -u $PC_UI_USER:$PC_UI_PASS \
+        --data "$REQUEST1"
+        )
 
-len=$(jq -r  '.group_results[0].entity_results | length' <<< "${response}" ) 
+len=$(jq -r  '.group_results[0].entity_results | length' <<< "${response}" )
 
 len=$(( 10#$len ))
 value=0
@@ -64,11 +70,11 @@ while [ $value -lt $len ]
 do
 
 echo "iterating $value"
-entity_id=$(jq -r  ".group_results[0].entity_results["$value"].entity_id" <<< "${response}" ) 
+entity_id=$(jq -r  ".group_results[0].entity_results["$value"].entity_id" <<< "${response}" )
 
 echo "Entity ID $entity_id"
 
-name=$(jq -r  ".group_results[0].entity_results["$value"].data[0].values[0].values[0]" <<< "${response}" ) 
+name=$(jq -r  ".group_results[0].entity_results["$value"].data[0].values[0].values[0]" <<< "${response}" )
 
 echo "Name $name"
 
@@ -79,7 +85,7 @@ REQUEST2=$(cat <<EOF
  "trigger_type": "incoming_webhook_trigger",
  "trigger_instance_list": [{
    "webhook_id": "$WEBHOOK_ID",
-   "entity1" : "{\"type\":\"$ORIGINAL_CT\",\"name\":\"$name\",\"uuid\":\"$entity_id\"}"
+   "entity1" : "{\"type\":\"$CATEGORY_TYPE\",\"name\":\"$name\",\"uuid\":\"$entity_id\"}"
  }]
 }
 
@@ -92,8 +98,8 @@ echo "$REQUEST2"
 TRIGGER_ENDPOINT="https://${PC_IP}:9440/api/nutanix/v3/action_rules/trigger"
 response=$(curl -X POST -k $TRIGGER_ENDPOINT \
         --header 'Content-Type: application/json' \
-        -u $PC_SSH_USER:$PC_SSH_PASS \
-        --data-raw "$REQUEST2" \
+        -u $PC_UI_USER:$PC_UI_PASS \
+        --data "$REQUEST2" \
         )
 
 echo "Updated successfully : $response"
