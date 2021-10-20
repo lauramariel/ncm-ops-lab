8663fbdc963838a1681cd3419cf2078bd0348b93
Sarah Hernandez <sarah.hernandez@nutanix.com>
2021-10-20 16:51:20 -0700

    Bugfixes in seedPC and add message to make it clear ticket page is not a part of the product

--- prismpro/lab/initialize_lab.sh
+++ prismpro/lab/initialize_lab.sh
@@ -7,11 +7,28 @@ PC_USER="$2"
 PC_PASS="$3"
 
 echo "Temporarily fixing NCC Bug that Prevents SeedPC from working"
-
+# ENG-432422 PART 1
+cp ~/ncc/lib/py/nutanix_serviceability-lib.egg nutanix_serviceability-lib-previous.egg
 cp nutanix_serviceability-lib.egg ~/ncc/lib/py/nutanix_serviceability-lib.egg
 
+
+# Update sizer timeout and restart sizer
+# https://jira.nutanix.com/browse/ENG-430580
+# Add DCBC_TIMEOUT_MS=1000 flag to the java call in sizer_service.sh
+source /etc/profile
+sed -i '/-DCBC_PATH*/a \  -DCBC_TIMEOUT_MS=1000 \\' /home/nutanix/neuron/bin/sizer_service.sh
+ps aux | grep sizer | grep java | grep -v grep | awk '{print $2}' > /home/nutanix/pid.txt
+
+# then kill sizer
+sleep 5
+kill -9 $(cat /home/nutanix/pid.txt)
+sleep 5
+
+# Stop cluster health for the NCC Bug ENG-432422
 genesis stop cluster_health
+# Stop neuron for both sizer and ncc bug
 genesis stop neuron
+# now restart the services
 cluster start
 
 echo "Enabling App Discovery and vCenter Monitoring Services"
@@ -31,11 +48,23 @@ python xfit_prismpro_write.py
 
 cd ../
 
+# ENG-432422 PART 2 replace the ncc egg back to prevent any issues now that the seeding is complete
+cp nutanix_serviceability-lib-previous.egg ~/ncc/lib/py/nutanix_serviceability-lib.egg
+
 echo "Seeding Application Discovery Data"
 
 cp mock_epoch_response.json ~/config/xdiscovery/mock_epoch_response.json
+
+# Wait for the discovery service to come up
+sleep 60
+
+echo "Now restart the Discovery Service"
 genesis stop dpm_server
 genesis stop xdiscovery
+# Stop cluster health and neuron for the NCC Bug ENG-432422
+genesis stop cluster_health
+genesis stop neuron
+# now restart the services
 cluster start
 sudo systemctl start iptables
 
